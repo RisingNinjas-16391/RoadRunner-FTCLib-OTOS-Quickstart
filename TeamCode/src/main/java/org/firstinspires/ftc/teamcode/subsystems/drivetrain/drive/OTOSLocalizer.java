0c8ee2006edfffe06f42b7f6f9a41f78046b5b27
RisingNinjas16391 <risingninjas16391@gmail.com>
2024-09-06 16:29:13 -0400

    Revert "Anay 6/9/24"
    This reverts commit 2d5c4a3c0b6a35d0288513c8e81ce9a825720ebe.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/drivetrain/drive/OTOSLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/drivetrain/drive/OTOSLocalizer.java
@@ -68,7 +68,7 @@ public class OTOSLocalizer implements Localizer {
         // clockwise (negative rotation) from the robot's orientation, the offset
         // would be {-5, 10, -90}. These can be any value, even the angle can be
         // tweaked slightly to compensate for imperfect mounting (eg. 1.3 degrees).
-        SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(0, 0, 90);
+        SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(0, 0, 0);
         myOtos.setOffset(offset);
 
         // Here we can set the linear and angular scalars, which can compensate for
